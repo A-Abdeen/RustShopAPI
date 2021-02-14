23fc4b91a2a3ec251b482eed061943652e00c20b
A-Abdeen <ahmed.yahya18@gmail.com>
2021-02-14 12:09:34 +0300

    03 - Product Create Backend

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 let cars = require("./cars");
+const bodyParser = require("body-parser");
+const slugify = require("slugify");
 
 const app = express();
 
+app.use(bodyParser.json());
+
+// FULL YARD
 app.get("/cars", (req, res) => res.json(cars));
 
+// SINGLE CAR DETAIL BY ID
 app.get("/cars/:carId", (req, res) => {
   const foundCar = cars.find((car) => car.id === +req.params.carId);
   if (foundCar) {
@@ -14,6 +20,7 @@ app.get("/cars/:carId", (req, res) => {
     res.json({ message: "Entry not found" });
   }
 }),
+  // DELETE CAR BY ID
   app.delete("/cars/:carId", (req, res) => {
     const foundCar = cars.find((car) => car.id === +req.params.carId);
     if (foundCar) {
@@ -24,6 +31,15 @@ app.get("/cars/:carId", (req, res) => {
       res.json({ message: "Entry not found" });
     }
   });
+// ADD CAR
+app.post("/cars", (req, res) => {
+  const id = cars[cars.length - 1].id + 1;
+  const slug = slugify(req.body.name, { lower: true });
+  const newCar = { id, slug, ...req.body };
+  cars.push(newCar);
+  res.status(201).json(newCar);
+});
+
 app.listen(8000, () =>
   console.log("why are you running, WHY ARE YOU RUNNING on localhost:8000 ? ")
 );
