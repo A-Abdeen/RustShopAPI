47783dd4ed2f19b27d6176894f640e847ecd4421
A-Abdeen <ahmed.yahya18@gmail.com>
2021-02-22 16:54:05 +0300

    14 - Fetch Product List Redux

--- app.js
+++ app.js
@@ -2,19 +2,33 @@ const express = require("express");
 const db = require("./db/models");
 const bodyParser = require("body-parser"); // Why do I need this?
 const slugify = require("slugify"); // Where is this being used?
-const extraRoutes = require("./routes/extra");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 const carRoutes = require("./routes/cars");
 const manufacturerRoutes = require("./routes/manufacturers");
+const userRoutes = require("./routes/users");
+const extraRoutes = require("./routes/extra");
+const { localStrategy } = require("./middleware/passport");
+const cors = require("cors");
 
 const path = require("path");
 const app = express();
 
+// Passport Setup
+app.use(passport.initialize());
+app.use(passport.initialize());
+passport.use(localStrategy);
+
 // Middleware---------------------------------
 app.use(bodyParser.json());
+app.use(cors());
+
+// ROUTES-------------------------------------
 app.use(extraRoutes);
 app.use("/cars", carRoutes);
 app.use("/manufacturers", manufacturerRoutes);
 app.use("/media", express.static(path.join(__dirname, "media")));
+app.use(userRoutes);
 
 // NOT FOUND----------------------------------
 app.use((req, res, next) => {
---
+++ db/models/User.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "ermm.. Username already exists",
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "This email address is already used",
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Must be standard email format (email@domain.com)",
+        },
+      },
+    },
+
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+  });
+  return User;
+};
